f64ae4f3ec881ed3251206b9b250bf2b1529ca18
Pater GitHelp <micbo26@gmail.com>
2020-05-29 22:50:20 +0200

    Jestem idiotą i nie potrafię pushować
    BRUHBRUHBRUH

--- Kopakabana/Finaly.cs
+++ Kopakabana/Finaly.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Kopakabana
 {
-    class Class1
+    class Finaly
     {
+        private Mecz m1;
+        private Mecz m2;
+        private Mecz m3;
+        private Druzyna d1;
+        private Druzyna d2;
+        private Druzyna d3;
+        private Druzyna d4;
+        private List<Druzyna> Drabina = List<Druzyna>();
+        private List<Sedzia> s = List<Sedzia>();
+        public Finaly(Druzyna d1, Druzyna d2, Druzyna d3, Druzyna d4, List<Sedzia> s)
+        {
+            this.d1 = d1;
+            this.d2 = d2;
+            this.d3 = d3;
+            this.d4 = d4;
+            this.s = s;
+        }
+        public void Start()
+        {
+
+        }
     }
 }
