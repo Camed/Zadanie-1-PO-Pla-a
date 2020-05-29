ab4b3d64604d4fb17ff8d89a81bb666473c06c7d
Pater GitHelp <micbo26@gmail.com>
2020-05-29 23:19:50 +0200

    Finaly
    Szczerze to nie wiem co dalej

--- Kopakabana/Finaly.cs
+++ Kopakabana/Finaly.cs
@@ -8,16 +8,26 @@ namespace Kopakabana
 {
     class Finaly
     {
-        private Mecz m1;
-        private Mecz m2;
-        private Mecz m3;
-        private Druzyna d1;
-        private Druzyna d2;
-        private Druzyna d3;
-        private Druzyna d4;
-        private List<Druzyna> Drabina = List<Druzyna>();
-        private List<Sedzia> s = List<Sedzia>();
-        public Finaly(Druzyna d1, Druzyna d2, Druzyna d3, Druzyna d4, List<Sedzia> s)
+        
+
+        public virtual void Start()
+        {
+            
+        }
+    }
+    class FinSiatka : Finaly
+    {
+        private MeczSiatkowki m1;
+        private MeczSiatkowki m2;
+        private MeczSiatkowki m3;
+        private MeczSiatkowki m4;
+        private DruzynaSiatkowka d1;
+        private DruzynaSiatkowka d2;
+        private DruzynaSiatkowka d3;
+        private DruzynaSiatkowka d4;
+        private List<Druzyna> Drabina = new List<Druzyna>();
+        private List<Sedzia> s;
+        public FinSiatka(DruzynaSiatkowka d1, DruzynaSiatkowka d2, DruzynaSiatkowka d3, DruzynaSiatkowka d4, List<Sedzia> s)
         {
             this.d1 = d1;
             this.d2 = d2;
@@ -25,8 +35,14 @@ namespace Kopakabana
             this.d4 = d4;
             this.s = s;
         }
-        public void Start()
+        public override void Start()
         {
+            m1 = new MeczSiatkowki(d1, d2, s[0], s[1], s[2]);
+            m1.Play();
+            m2 = new MeczSiatkowki(d3, d4, s[3], s[4], s[5]);
+            m2.Play();
+            m3= new MeczSiatkowki(m1.Zwyciezca,m2.Zwyciezca, s[0], s[1], s[2])
+            
 
         }
     }
