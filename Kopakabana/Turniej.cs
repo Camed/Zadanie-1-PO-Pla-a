dab2e1461d660b605a918d21b38f0224f8009e05
Łukasz <chojecki2000@gmail.com>
2020-05-29 23:56:40 +0200

    hi

--- Kopakabana/Turniej.cs
+++ Kopakabana/Turniej.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Kopakabana
 {
-    class Turniej
+    abstract class Turniej
     {
+        protected List<Mecz> mecze = new List<Mecz>();
+        protected List<Druzyna> druzyna = new List<Druzyna>();
+        protected List<Sedzia> sedzie = new List<Sedzia>();
+
+        public abstract void Start();
     }
 }
